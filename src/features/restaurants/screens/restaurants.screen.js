e338e01edbc34c13a62d3b87c8c94f4e63f803f6
Roger Long <docrogerlong@gmail.com>
2022-07-23 16:20:42 -0500

    20-flatlist added

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,8 +1,10 @@
 import React from "react";
-import styled from "styled-components";
 import { Searchbar } from "react-native-paper";
-import { SafeAreaView, StatusBar } from "react-native";
+import { StatusBar, FlatList, SafeAreaView } from "react-native";
+import styled from "styled-components/native";
+
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
@@ -24,7 +26,31 @@ export const RestaurantScreen = () => (
       <Searchbar />
     </SearchContainter>
     <RestaurantListContainer>
-      <RestaurantInfoCard />
+      <FlatList
+        data={[
+          { name: 1 },
+          { name: 2 },
+          { name: 3 },
+          { name: 4 },
+          { name: 5 },
+          { name: 6 },
+          { name: 7 },
+          { name: 8 },
+          { name: 9 },
+          { name: 10 },
+          { name: 11 },
+          { name: 12 },
+          { name: 13 },
+          { name: 14 },
+        ]}
+        renderItem={() => (
+          <Spacer position="bottom" size="large">
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ padding: 16 }}
+      />
     </RestaurantListContainer>
   </SafeArea>
 );
