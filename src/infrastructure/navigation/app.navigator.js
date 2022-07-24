73a747d35f09e0069b878efb972b91d9004190a3
Roger Long <docrogerlong@gmail.com>
2022-07-24 17:40:18 -0500

    44-Maps screen created

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -7,6 +7,7 @@ import { Text } from "react-native";
 import { SafeArea } from "../../components/utility/safe-area.component";
 
 import { RestaurantsNavigator } from "./restaurants.navigator";
+import { MapScreen } from "../../features/map/screens/map.screen";
 
 const Tab = createBottomTabNavigator();
 
@@ -21,11 +22,6 @@ const Settings = () => (
         <Text>Settings</Text>
     </SafeArea>
 );
-const Map = () => (
-    <SafeArea>
-        <Text>Map</Text>
-    </SafeArea>
-);
 
 const createScreenOptions = ({ route }) => {
     const iconName = TAB_ICON[route.name];
@@ -38,9 +34,15 @@ const createScreenOptions = ({ route }) => {
 
 export const AppNavigator = () => (
     <NavigationContainer>
-        <Tab.Navigator screenOptions={createScreenOptions}>
+        <Tab.Navigator
+            screenOptions={createScreenOptions}
+            tabBarOptions={{
+                activeTintColor: "tomato",
+                inactiveTintColor: "gray",
+            }}
+        >
             <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-            <Tab.Screen name="Map" component={Map} />
+            <Tab.Screen name="Map" component={MapScreen} />
             <Tab.Screen name="Settings" component={Settings} />
         </Tab.Navigator>
     </NavigationContainer>
